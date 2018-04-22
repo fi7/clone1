bf6754383e3f8a2b67a9debc07f1ec6c7515b906
Filippos <gkafras7@gmail.com>
2018-04-22 16:11:58 +0300

    Add postcss packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,34 @@
 const gulp = require('gulp'),
-      watch = require('gulp-watch');
+      watch = require('gulp-watch'),
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      cssvars = require('postcss-simple-vars'),
+      nested = require('postcss-nested');
 
 
 
+gulp.task('html', function () {
 
+});
+
+
+gulp.task('styles', function() {
+   return gulp.src('./app/assets/styles/main.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
 
 
 //Watch
 gulp.task('watch', function() {
   
-  
+  watch('./app/index.html', function () {
+    gulp.start('html');
+  });
+
+
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
+  });
 
 });
